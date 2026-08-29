519d49bbff611d015563c43c2f03348e2f8bd9e5
AbdelRhman <abdelrhman@MacBook-air.local>
2026-08-29 16:23:49 +0300

    Fix Supabase package import

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,6 +1,6 @@
-import "dotenv/config";
-import { createClient } from "@Supabase/supabase-js";
 
+import "dotenv/config";
+import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SECRET_KEY = process.env.SUPABASE_SECRET_KEY;
 
